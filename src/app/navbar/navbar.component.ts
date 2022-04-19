117883faac2d3309146edf122c5ce5765f362788
Ngina-G <Nginagitau11@outlook.com>
2022-04-19 12:25:06 +0300

    update: adds user class

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { UserService } from 'src/app/services/user.service';
 import { RequestLimitService } from 'src/app/services/request-limit.service';
-import { User } from 'src/app/models/User';
+import { User } from 'src/app/User';
 
 @Component({
   selector: 'app-navbar',
---
+++ src/app/user.ts
@@ -0,0 +1,13 @@
+export interface User {
+    avatar_url: string;
+    login: string;
+    followers: number;
+    following: number;
+    bio: string;
+    twitter_username: string;
+    public_repos: number;
+    location: string;
+    updated_at: any;
+    hireable: boolean;
+    html_url: string;
+  }
