0a2a750a522f69ab73361ee0dca7a087a0058cf8
Ngina-G <Nginagitau11@outlook.com>
2022-04-19 12:14:00 +0300

    update:adds navbar

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+import { UserService } from 'src/app/services/user.service';
+import { RequestLimitService } from 'src/app/services/request-limit.service';
+import { User } from 'src/app/models/User';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+})
+export class NavbarComponent implements OnInit {
+  requests: any = {};
+  user!: User;
+
+  constructor(
+    private userService: UserService,
+    private requestLimitService: RequestLimitService
+  ) {}
+
+  ngOnInit(): void {
+    this.userService.user.subscribe((user) => (this.user = user));
+    this.requestLimitService.getRequestLimit().subscribe((response) => {
+      this.requests.limit = response.resources.core.limit;
+      this.requests.used = response.resources.core.used;
+      this.requests.remaining = response.resources.core.remaining;
+    });
+  }
+}
